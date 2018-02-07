3f858c47b08720c703c9f31131dab2bad4ba553d
blancams <blancamartinselgas@gmail.com>
2018-02-07 23:26:54 +0100

    Cosas que no funcionan

--- P1/p1-ej1235.lisp
+++ P1/p1-ej1235.lisp
@@ -79,8 +79,6 @@
 
 
 
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; EJERCICIO 2 ;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Finds a root of f between the points a and b using bisection.
@@ -100,5 +98,39 @@
    (cond    ((= (funcall f pto-medio) 0) pto-medio)
          ((> (* (funcall f a) (funcall f b)) 0) NIL)
          ((< (- b a) tol) pto-medio)
-         ((> (* (funcall f a) (funcall f pto-medio)) 0) (bisect f pto-medio b (/ tol 2))) 
-         (t (bisect f a pto-medio (/ tol 2))))))
+         ((> (* (funcall f a) (funcall f pto-medio)) 0) (bisect f pto-medio b tol)) 
+         (t (bisect f a pto-medio tol)))))
+
+
+
+(defun allroot-aux (f lst tol ret)
+   (cond ((and (null (rest lst)) (not (null (first lst)))) ret)
+        (t (append ret (bisect f (first lst) (second lst) tol))
+        (allroot-aux f (rest lst) tol ret))))
+
+;;
+;; Finds all the roots that are located between consecutive values of a list
+;; of values
+;;
+;; Parameters:
+;;
+;; f: function of a single real parameter with real values whose root
+;; we want to find
+;; lst: ordered list of real values (lst[i] < lst[i+1])
+;; tol: tolerance for the stopping criterion: if b-a < tol the function
+;; returns (a+b)/2 as a solution.
+;;
+;; Whenever sgn(f(lst[i])) != sgn(f(lst[i+1])) this function looks for a
+;; root in the corresponding interval.
+;;
+;; Returns: A list o real values containing the roots of the function in the
+;: given sub-intervals
+;;
+(defun allroot (f lst tol)
+   (allroot-aux f lst tol NIL))
+
+;;(allroot #'(lambda(x) (sin (* 6.28 x))) '(0.25 0.75 1.25 1.75 2.25) 0.0001)
+;; Para ver si tiene solo un elemento: 
+;; (and (null (rest lst)) (not (null (first lst))))
+;;(allroot #'(lambda(x) (sin (* 6.26 x))) '(0.25 0.75) 0.001)
+;;(bisect #'(lambda(x) (sin (* 6.28 x))) 1.1 2.1 0.001)
