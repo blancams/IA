ead2f1b8aa170e2f449ff42d6d02353c2d08f912
blancams <blancamartinselgas@gmail.com>
2018-02-09 11:23:31 +0100

    Cosas

--- P1/p1-ej1235.lisp
+++ P1/p1-ej1235.lisp
@@ -268,11 +268,20 @@
 (defun combine-elt-lst (elt lst)
    (if (null lst)
       NIL
-      (append (list (list elt (first lst))) (combine-elt-lst elt (rest lst)))))
+      (if (check-list elt)
+         (append (list (append elt (list (first lst)))) (combine-elt-lst elt (rest lst)))
+         (append (list (list elt (first lst))) (combine-elt-lst elt (rest lst))))))
 
 (defun combine-lst-lst (lst1 lst2)
    (if (null lst1)
       NIL
       (append (combine-elt-lst (first lst1) lst2) (combine-lst-lst (rest lst1) lst2))))
 
+(defun combine-list-of-lsts-aux (done todo)
+   (if (null todo)
+      done
+      (combine-list-of-lsts-aux (combine-lst-lst done (first todo)) (rest todo))))
+
+(defun combine-list-of-lsts (lstolsts) 
+   (combine-list-of-lsts-aux (combine-lst-lst (first lstolsts) (second lstolsts)) (cddr lstolsts)))
 
