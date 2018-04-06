da399f8be797645302d8237febfe5124afcf5375
SLiezzan <sliezan19@gmail.com>
2018-04-06 12:55:55 +0200

    Hechos 1 y 2 de la práctica 3

--- P3/2302_p3_03.pl
+++ P3/2302_p3_03.pl
@@ -1,2 +1,12 @@
 pertenece(X, [X|_]).
 pertenece(X, [_|Rs]) :- pertenece(X, Rs).
+
+pertenece_m(X, [X|_]) :- X \= [_|_].
+pertenece_m(X, [Rs|_]) :- pertenece_m(X, Rs).
+pertenece_m(X, [_|Rs]) :- pertenece_m(X, Rs).
+
+concatena([], L, L).
+concatena([X|L1], L2, [X|L3]) :- concatena(L1, L2, L3).
+
+invierte([], []).
+invierte([X|L1], L2) :- invierte(L1, M), concatena(M, [X], L2).
